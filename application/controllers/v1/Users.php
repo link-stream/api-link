4c3c72cd52ad197f7c7990f0df24896f405e83b7
Paul Ferra <paul_ferra@ip-192-168-0-26.us-east-2.compute.internal>
2020-02-25 19:00:33 -0500

    API

--- application/controllers/v1/Users.php
+++ application/controllers/v1/Users.php
@@ -21,6 +21,8 @@ class Users extends RestController {
         $this->load->model("User_model");
         //Libraries
         $this->load->library(array('Instagram_api', 'aws_s3', 'Aws_pinpoint'));
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     }
 
 //    public function _remap($method, $arguments = array()) {
