323dbbcbb3b942c91a51a7e5cca3db6c0fb3dbc1
Paul Ferra <paul_ferra@ip-192-168-0-26.us-east-2.compute.internal>
2020-02-25 22:36:23 -0500

    TEst

--- application/views/app/api_test.php
+++ application/views/app/api_test.php
@@ -7,7 +7,7 @@ $data['page'] = 'Dashboard';
 
 <body class="text-left">
     <div class="app-admin-wrap layout-sidebar-large">
-       Test: 9
+       Test: 10
     </div>
 
     <?php $this->load->view('app/_inc/footer', $data); ?>
