4e29309eb53efc8dd0d2f9e766f2e6c583118812
Paul Ferra <paul_ferra@ip-192-168-0-26.us-east-2.compute.internal>
2020-06-06 21:26:19 -0400

    Audios

--- application/controllers/v1/Audios.php
+++ application/controllers/v1/Audios.php
@@ -208,9 +208,9 @@ class Audios extends RestController {
 
     public function index_get($id = null, $track_type = null, $audio_id = null) {
         if (!empty($id)) {
-//            if (!$this->general_library->header_token($id)) {
-//                $this->response(array('status' => 'false', 'env' => ENV, 'error' => 'Unauthorized Access!'), RestController::HTTP_UNAUTHORIZED);
-//            }
+            if (!$this->general_library->header_token($id)) {
+                $this->response(array('status' => 'false', 'env' => ENV, 'error' => 'Unauthorized Access!'), RestController::HTTP_UNAUTHORIZED);
+            }
             $page = (!empty($this->input->get('page'))) ? intval($this->input->get('page')) : 0;
             $page_size = (!empty($this->input->get('page_size'))) ? intval($this->input->get('page_size')) : 0;
             //$limit = 0;
