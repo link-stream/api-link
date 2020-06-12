f7b4fd61b6b51174428cc34d0f908f0f719ad44a
Paul Ferra <paul_ferra@ip-192-168-0-26.us-east-2.compute.internal>
2020-06-12 10:14:32 -0400

    Audio Updated

--- application/models/Audio_model.php
+++ application/models/Audio_model.php
@@ -48,7 +48,7 @@ class Audio_model extends CI_Model {
         if (!empty($search['excluded_id'])) {
             $this->db->where('id <> ', $search['excluded_id']); //By Usew
         }
-        $this->db->where('status_id <>', 3); //By Status
+        $this->db->where('status_id <> ', '3');
         if (!empty($limit)) {
             $this->db->limit($limit, $offset);
         }
