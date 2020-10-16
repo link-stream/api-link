1b5cc8009aae1a1fafb19ab3032bf76298b1690a
Paul Ferra <paul_ferra@ip-192-168-0-13.us-east-2.compute.internal>
2020-10-16 18:46:11 -0400

    Action Open

--- application/controllers/v1/A.php
+++ application/controllers/v1/A.php
@@ -7,8 +7,8 @@ class A extends CI_Controller {
     public function __construct() {
         parent::__construct();
         //Models
-        $this->load->model("User_model");
-        $this->load->model("Streamy_model");
+        $this->load->model("Marketing_model");
+        //$this->load->model("Streamy_model");
     }
 
     public function _remap($method, $params = array()) {
@@ -32,20 +32,10 @@ class A extends CI_Controller {
 
     public function action_open() {
         if (!empty($this->input->get('ref_id'))) {
-            $id = $this->input->get('ref_id');
-//            $pos = strpos($id, '_');
-//            $log_id = substr($id, 0, $pos);
-//            $ref_id = substr($id, $pos + 1);
-            //SET LATER
-//            $detail = $this->c_notification_model->get_notification_by_ref($log_id, $ref_id);
-//            if (empty($detail)) {
-//                $this->c_notification_model->insert_notification_detail(array('log_id' => $log_id, 'ref_id' => $ref_id));
-//            }
+            $ref_id = $this->input->get('ref_id');
+            $this->Marketing_model->update_open_action($ref_id);
         }
         $imagen_url = (ENV != 'live') ? 'https://dev-link-vue.link.stream/static/img/open.jpg' : 'https://linkstream/static/img/open.jpg';
-//        print_r(ENV);
-//        echo $id;
-//        exit;
         header("Content-Type: image/jpeg"); // it will return image 
         $logo = file_get_contents($imagen_url);
         echo $logo;
