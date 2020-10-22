6e1eb857820716bd5c06656778560def86dd81f5
Paul Ferra <paul_ferra@ip-192-168-0-13.us-east-2.compute.internal>
2020-10-22 17:01:23 -0400

    Config controller

--- application/controllers/v1/Config.php
+++ application/controllers/v1/Config.php
@@ -38,7 +38,7 @@ class Config extends RestController {
     public function fees_get() {
 
         $data['fees'] = [
-            ['name' => 'Credit Card Fee', 'type' => 'Percent', 'value' => '3', 'var' => 'feeCC'],
+            ['name' => 'Convenience Fee', 'type' => 'Percent', 'value' => '3', 'var' => 'feeCC'],
             ['name' => 'Service Fee', 'type' => 'Amount', 'value' => '4.99', 'var' => 'feeService'],
         ];
         $this->response(array('status' => 'success', 'env' => ENV, 'data' => $data), RestController::HTTP_OK);
