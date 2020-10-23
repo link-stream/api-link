96fcb5d0ca25c66f2f0b5881e30f58e95337badc
Paul Ferra <paul_ferra@ip-192-168-0-13.us-east-2.compute.internal>
2020-10-23 10:44:12 -0400

    Testing cron

--- application/controllers/v1/C_notification.php
+++ application/controllers/v1/C_notification.php
@@ -212,8 +212,6 @@ class C_notification extends CI_Controller {
         echo date("Y-m-d H:i:s");
         echo '<br>';
         echo date("Y-m-d H:i:s");
-         echo '<br>';
-        echo date("Y-m-d H:i:s");
     }
 
     public function ruta() {
