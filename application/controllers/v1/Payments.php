7b6e9df7861f6f57c7572c976d7c0f8c49b6ebef
Paul Ferra <paul_ferra@ip-192-168-0-13.us-east-2.compute.internal>
2020-10-27 11:02:13 -0400

    Adding Title

--- application/controllers/v1/Payments.php
+++ application/controllers/v1/Payments.php
@@ -597,6 +597,7 @@ class Payments extends RestController {
                                 $item_response['avatar_url'] = $final_url;
                             }
                             $item_response['artist_url'] = $item_audio['url'];
+                            $item_response['title'] = $item_audio['title'];
                             $data_response[] = $item_response;
                         }
                     }
